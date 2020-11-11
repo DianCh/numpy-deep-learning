c62c31f10796b365e306caf6ffb033d2ee94b2a3
Dian Chen <namedcd@gmail.com>
2020-11-11 01:22:42 -0800

    schedulers in progress

---
+++ scheduler.py
@@ -0,0 +1,14 @@
+class StepLR:
+    def __init__(self, optimizer, step_size, gamma=0.1, last_epoch=-1):
+        pass
+
+    def step(self):
+        pass
+
+
+class MultiStepLR:
+    def __init__(self, optimizer, milestones, gamma=0.1, last_epoch=-1):
+        pass
+
+    def step(self):
+        pass
