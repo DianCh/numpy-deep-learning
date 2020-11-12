ccb1e433745ce3d93c064fadb66889b5ee8349b4
Dian Chen <namedcd@gmail.com>
2020-11-12 14:49:33 -0800

    started MLP

---
+++ model.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+from layer import Conv2D, Linear, ReLU, Pool2D, Squeeze2D
+
+
+class MultiLayerPerceptron:
+    def __init__(self, in_features, out_features, hidden):
+        self.channels = [in_features, *hidden, out_features]
+
+        self.layers = []
+        for l in range(len(self.channels) - 1):
+            linear = Linear(self.channels[l], self.channels[l + 1], bias=True)
+            relu = ReLU()
+
+            self.layers.append(linear)
+            self.layers.append(relu)
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+
+        return x
