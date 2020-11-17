971830b201ce876ea498c772d5b098c08760d472
Dian Chen <namedcd@gmail.com>
2020-11-16 23:07:34 -0800

    added momentum in SGD

--- ndl/optimizer.py
+++ ndl/optimizer.py
@@ -2,18 +2,28 @@ import numpy as np
 
 
 class SGD:
-    def __init__(self, model, lr=0.01):
-        self.model = model
+    def __init__(self, model, lr=0.01, momentum=0):
+        self.layers = []
+        self.velocities = []
+        for layer in model.layers:
+            if hasattr(layer, "dW") and hasattr(layer, "db"):
+                self.layers.append(layer)
+                self.velocities.append(
+                    {"dW": np.zeros((layer.dW.shape)), "db": np.zeros((layer.db.shape))}
+                )
+
         self.lr = lr
+        self.momentum = momentum
 
     def zero_grad(self):
-        for layer in self.model.layers:
+        for layer in self.layers:
             if hasattr(layer, "dW") and hasattr(layer, "db"):
                 layer.clear_gradients()
 
     def step(self):
-        for layer in self.model.layers:
-            if hasattr(layer, "dW") and hasattr(layer, "db"):
-                layer.W -= self.lr * layer.dW
-                layer.b -= self.lr * layer.db
+        for layer, velocity in zip(self.layers, self.velocities):
+            velocity["dW"] = self.momentum * velocity["dW"] + layer.dW
+            velocity["db"] = self.momentum * velocity["db"] + layer.db
+            layer.W -= self.lr * velocity["dW"]
+            layer.b -= self.lr * velocity["db"]
 
